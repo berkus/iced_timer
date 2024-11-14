b8685b6012574d2178d31169c8f5272f1d31e1c5
Berkus Decker <berkus+github@metta.systems>
2024-11-14 23:47:07 +0200

    feat: ✨ Add error indication

--- src/input_view.rs
+++ src/input_view.rs
@@ -1,7 +1,9 @@
 use iced::{
     alignment::Horizontal,
+    border::Radius,
+    theme,
     widget::{button, column, container, row, text, text_input},
-    Element, Task,
+    Border, Color, Element, Length, Task,
 };
 
 // three similar widgets in panel
@@ -19,6 +21,7 @@ pub struct InputView {
     value: u8,
     label: &'static str,
     clamp: Option<(u8, u8)>,
+    error: bool,
 }
 
 impl InputView {
@@ -27,6 +30,7 @@ impl InputView {
             value: init_value.unwrap_or(0),
             label,
             clamp: None,
+            error: false,
         }
     }
 
@@ -48,30 +52,48 @@ impl InputView {
     }
 
     pub fn view(&self) -> Element<Message> {
-        column![
+        let c = column![
             text(self.label),
             button("+").on_press(Message::NewValue(self.clamp(self.value.saturating_add(1)))),
-            text_input("0", &format!("{}", self.value)).on_input(|s| {
-                let v = s.parse::<u8>();
-                if v.is_ok() {
-                    Message::NewValue(self.clamp(v.unwrap()))
-                } else {
-                    Message::ErrorInput
-                }
-            }),
+            text_input("0", &format!("{}", self.value))
+                .on_input(|s| {
+                    let v = s.parse::<u8>();
+                    if v.is_ok() {
+                        Message::NewValue(self.clamp(v.unwrap()))
+                    } else {
+                        Message::ErrorInput
+                    }
+                })
+                .size(20)
+                .width(Length::FillPortion(1)),
             button("-").on_press(Message::NewValue(self.clamp(self.value.saturating_sub(1))))
         ]
         .align_x(Horizontal::Center)
-        .into()
+        .width(Length::Shrink);
+
+        if self.error {
+            container(c)
+                .style(|theme| {
+                    container::bordered_box(theme).border(Border {
+                        color: Color::new(1.0, 0.0, 0.0, 0.5),
+                        radius: Radius::new(5.0),
+                        width: 3.0,
+                    })
+                })
+                .into()
+        } else {
+            container(c).into()
+        }
     }
 
     pub fn update(&mut self, message: Message) -> Task<Message> {
         match message {
             Message::ErrorInput => {
-                // show error
+                self.error = true;
             }
             Message::NewValue(v) => {
                 self.value = v;
+                self.error = false;
             }
         }
         Task::none()
